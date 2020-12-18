d321c621b8283815ad8eabdcc881871235d70da3
Anusha Shet <anushaanil44@gmail.com>
2020-12-18 15:52:41 +0530

    removed other methods

--- DemoApp/src/com/lti/model/Calci.java
+++ DemoApp/src/com/lti/model/Calci.java
@@ -1,18 +1,6 @@
 package com.lti.model;
 
 public class Calci {
-	public void addmethod(int n1, int n2) {
-		int n3 = n1 + n2;
-		System.out.println("Inside a add method");
-	}
-	public void submethod(int n1, int n2) {
-		int n3 = n1 - n2;
-		System.out.println(n3);
-	}
-	public void mulmethod(int n1, int n2) {
-		int n3 = n1 * n2;
-		System.out.println("Inside a mul method");
-	}
 	public void divmethod(int n1, int n2) {
 		int n3 = n1/n2;
 		System.out.println("Inside a div method");
